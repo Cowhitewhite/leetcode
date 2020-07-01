092ebc467b12ee6155e35b317abd66f10281c869
Cowhitewhite <960095492@qq.com>
2020-07-01 15:46:00 +0800

    新增

---
+++ src/leetcode/editor/cn/UzipNum.java
@@ -0,0 +1,29 @@
+package leetcode.editor.cn;
+/*
+HG[3|B[2|CA]]F -> HG[3|BCACA]F -> HGBCACABCACABCACAF
+ */
+
+import java.util.Collections;
+
+/**
+ * @Classname UzipNum
+ * @Description TODO
+ * @Date 2020/7/1 15:43
+ * @Created by ChenHao
+ */
+public class UzipNum {
+    public static void main(String[] args) {
+        System.out.println(decode("HG[3|B[2|CA]]F"));
+    }
+
+    public static String decode(String words){
+        while (words.contains("]")){
+            int right = words.indexOf("]");
+            int left = words.lastIndexOf("[",right);
+            String temp = words.substring(left+1,right);
+            String[] splitTemp = temp.split("\\|");
+            words = words.replace("["+temp+"]",String.join("", Collections.nCopies(Integer.parseInt(splitTemp[0]),splitTemp[1])));
+        }
+        return words;
+    }
+}

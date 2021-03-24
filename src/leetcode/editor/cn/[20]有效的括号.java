b4846277dd6ca43fa80881d2a0fccdf294e21e1f
陈浩 <y94_t1dfvd6de@dingtalk.com>
2021-03-24 16:07:59 +0800

    解法增加

--- src/leetcode/editor/cn/[20]有效的括号.java
+++ src/leetcode/editor/cn/[20]有效的括号.java
@@ -45,8 +45,8 @@ import java.util.Stack;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution6{
     public static void main(String[] args) {
-        String brackets = "{[}]";
-        System.out.println(isValid(brackets));
+        String brackets = "{}[]";
+        System.out.println(isValid2(brackets));
     }
     public static boolean isValid(String s) {
 
@@ -71,6 +71,30 @@ class Solution6{
             }
         }
         return stack.empty();
+    }
+
+    public static boolean isValid2(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if ('(' == c || '[' == c || '{' == c){
+                stack.push(c);
+            }else {
+                if (stack.isEmpty()){
+                    return false;
+                }
+                char pop = stack.pop();
+                if (c == ')' && pop != '('){
+                    return false;
+                }
+                if (c == ']' && pop != '['){
+                    return false;
+                }
+                if (c == '}' && pop != '{'){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
